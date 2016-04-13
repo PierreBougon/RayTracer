ef2a5dc1b9593a82b5a55222b9347e8652f44298
Marc <brout.marc@gmail.com>
2016-04-13 13:49:27 +0200

    clean

---
+++ include/solver.h
@@ -0,0 +1,16 @@
+/*
+** solver.h for raytracer in ~/RENDU/INFOGRAPHIE/gfx_raytracer2
+**
+** Made by marc brout
+** Login   <brout_m@epitech.net>
+**
+** Started on  Wed Apr 13 13:46:56 2016 marc brout
+** Last update Wed Apr 13 13:49:22 2016 marc brout
+*/
+
+#ifndef SOLVER_H_
+# define SOLVER_H_
+
+
+
+#endif /* !SOLVER_H_ */
---
+++ solvers/second_order_solver.c
@@ -0,0 +1,9 @@
+/*
+** second_order_solver.c for raytracer2
+**
+** Made by marc brout
+** Login   <brout_m@epitech.net>
+**
+** Started on  Wed Apr 13 13:45:54 2016 marc brout
+** Last update Wed Apr 13 13:46:18 2016 marc brout
+*/
